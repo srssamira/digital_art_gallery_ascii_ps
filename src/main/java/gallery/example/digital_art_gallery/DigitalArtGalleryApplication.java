542850c47de96a704931f94cb8deb85150b70de4
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2025-10-15 19:06:52 -0300

    HIS-15-10-2025: Estruturacao da arquitetura e dependencias

---
+++ src/main/java/gallery/example/digital_art_gallery/DigitalArtGalleryApplication.java
@@ -0,0 +1,13 @@
+package gallery.example.digital_art_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalArtGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalArtGalleryApplication.class, args);
+	}
+
+}
